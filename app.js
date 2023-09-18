c9ad82c270a9fbcf9a09b1e6c1488d1e8fde9605
Anton Chepko <choptonys@gmail.com>
2023-09-18 22:46:06 +0200

    2. 18.09.2023 - CRUD - get all managers

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+
 const express = require('express')
+
+const managers = [
+    {name: 'Anton', age: 22},
+    {name: 'Anna', age: 22},
+    {name: 'Vadym', age: 22},
+]
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req, res)=>{
+    res.send({
+        message:'hello'
+    })
+})
+app.get('/managers',(req,res)=>{
+    res.status(200).json({message:'All users',data:managers})
+})
+
+
+const PORT = 5001
+app.listen(PORT, () => {
+    console.log(`Server has started on PORT ${PORT}`)
+})
