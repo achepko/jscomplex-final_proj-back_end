551f85fddac465fb5a415e871d4a958039590440
Anton Chepko <choptonys@gmail.com>
2023-09-18 23:21:14 +0200

    4. 18.09.2023 - Eslint has added to project

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,26 @@
-import express,{Request,Response} from "express"
+import express, { Request, Response } from "express";
 
 const managers = [
-    {name: 'Anton', age: 22},
-    {name: 'Anna', age: 22},
-    {name: 'Vadym', age: 22},
-]
+  { name: "Anton", age: 22 },
+  { name: "Anna", age: 22 },
+  { name: "Vadym", age: 22 },
+];
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // app.get('/',(req, res)=>{
 //     res.send({
 //         message:'hello'
 //     })
 // })
-app.get('/managers',(req:Request,res:Response)=>{
-    res.status(200).json(managers)
-})
+app.get("/managers", (req: Request, res: Response) => {
+  res.status(200).json(managers);
+});
 
-
-const PORT = 5001
+const PORT = 5001;
 app.listen(PORT, () => {
-    console.log(`Server has started on PORT ${PORT}`)
-})
+  console.log(`Server has started on PORT ${PORT}`);
+});
